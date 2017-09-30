03832ea11e77c9410761f30533c5ce749fb4b59f
Chad Carbert <chadcarbert@me.com>
2017-09-30 23:36:40 +0200

    Update quotes.js to use commonjs instead of es6 module export

--- quotes.js
+++ quotes.js
@@ -1,7 +1,7 @@
-export default [
+module.exports = [
   `There’s a million things I haven’t done, just you wait`,
   `If you stand for nothing, Burr, what’ll you fall for?`,
-  `I’m past patiently waitin’. I’m passionately smashin’ every expectation. Every action’s an act of creation! I’m laughin’ in the face of casualties and sorrow. For the first time, I’m thinkin’ past tomorrow`,
+  `I’m past patiently waitin. I’m passionately smashin every expectation. Every action’s an act of creation! I’m laughin’ in the face of casualties and sorrow. For the first time, I’m thinkin’ past tomorrow`,
   `I will send a fully armed battalion to remind you of my love! `,
   `Why do you write like you’re writing out of time?`,
   `Your debts are paid cuz you don’t pay for labor, “We plant seeds in the South. We create.” Yeah, keep ranting. We know who’s really doing the planting`,
