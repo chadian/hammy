b7f53b9de4db89a8e80e5b5ba4123354fe00fe54
Chad Carbert <chadcarbert@me.com>
2018-01-25 21:26:21 +0100

    Make specific speaker request stub factory based on speaker

--- tests/stubs/specific-speaker-request.js
+++ tests/stubs/specific-speaker-request.js
@@ -1,45 +1,47 @@
-{
-  "session": {
-    "new": true,
-    "sessionId": "SessionId",
-    "application": {
-      "applicationId": "amzn1.ask.skill.a8272edd-19a6-40b8-b233-3ca6e980b98d"
+module.exports = (speaker) => {
+  return {
+    "session": {
+      "new": true,
+      "sessionId": "SessionId",
+      "application": {
+        "applicationId": "amzn1.ask.specificrequesttestrun"
+      },
+      "attributes": {},
+      "user": {
+        "userId": "amzn1.ask.account"
+      }
     },
-    "attributes": {},
-    "user": {
-      "userId": "amzn1.ask.account"
-    }
-  },
-  "request": {
-    "type": "IntentRequest",
-    "requestId": "EdwRequestId",
-    "intent": {
-      "name": "SpecificSpeakerQuoteIntent",
-      "slots": {
-        "Speaker": {
-          "name": "Speaker",
-          "value": speaker
+    "request": {
+      "type": "IntentRequest",
+      "requestId": "EdwRequestId",
+      "intent": {
+        "name": "SpecificSpeakerQuoteIntent",
+        "slots": {
+          "Speaker": {
+            "name": "Speaker",
+            "value": speaker
+          }
+        }
+      },
+      "locale": "en-US",
+      "timestamp": "2018-01-25T07:19:40Z"
+    },
+    "context": {
+      "AudioPlayer": {
+        "playerActivity": "IDLE"
+      },
+      "System": {
+        "application": {
+          "applicationId": "amzn1.ask.skill"
+        },
+        "user": {
+          "userId": "amzn1.ask.account"
+        },
+        "device": {
+          "supportedInterfaces": {}
         }
       }
     },
-    "locale": "en-US",
-    "timestamp": "2018-01-25T07:19:40Z"
-  },
-  "context": {
-    "AudioPlayer": {
-      "playerActivity": "IDLE"
-    },
-    "System": {
-      "application": {
-        "applicationId": "amzn1.ask.skill"
-      },
-      "user": {
-        "userId": "amzn1.ask.account"
-      },
-      "device": {
-        "supportedInterfaces": {}
-      }
-    }
-  },
-  "version": "1.0"
-}
+    "version": "1.0"
+  }
+};
