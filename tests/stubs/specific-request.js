bf9ce63520cc8dbf6cc3fe8387f2628034004f0f
Chad Carbert <chadcarbert@me.com>
2018-01-25 08:28:18 +0100

    Add stub for a specific request

---
+++ tests/stubs/specific-request.js
@@ -0,0 +1,45 @@
+{
+  "session": {
+    "new": true,
+    "sessionId": "SessionId"
+    "application": {
+      "applicationId": "amzn1.ask.skill.a8272edd-19a6-40b8-b233-3ca6e980b98d"
+    },
+    "attributes": {},
+    "user": {
+      "userId": "amzn1.ask.account"
+    }
+  },
+  "request": {
+    "type": "IntentRequest",
+    "requestId": "EdwRequestId",
+    "intent": {
+      "name": "SpecificSpeakerQuoteIntent",
+      "slots": {
+        "Speaker": {
+          "name": "Speaker",
+          "value": "Hamilton"
+        }
+      }
+    },
+    "locale": "en-US",
+    "timestamp": "2018-01-25T07:19:40Z"
+  },
+  "context": {
+    "AudioPlayer": {
+      "playerActivity": "IDLE"
+    },
+    "System": {
+      "application": {
+        "applicationId": "amzn1.ask.skill"
+      },
+      "user": {
+        "userId": "amzn1.ask.account"
+      },
+      "device": {
+        "supportedInterfaces": {}
+      }
+    }
+  },
+  "version": "1.0"
+}
